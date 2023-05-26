99da2dd9588c50303fa53025addd7a8c0acdbe89
bmuraya <babumuraya@gmail.com>
2023-05-26 11:06:00 +0300

    entry point of shell Program

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 void data_free(data_shell *shell_data)
 {
 	unsigned int i;
+
 	i = 0;
+
 	while (shell_data->_environ[i])
 	{
 		free(shell_data->_environ[i]);
@@ -28,8 +30,7 @@ void data_free(data_shell *shell_data)
  */
 void data_set(data_shell *shell_data, char **av)
 {
-	unsigned int i;
-	i = 0;
+	unsigned int i = 0;
 
 	shell_data->av = av;
 	shell_data->input = NULL;
